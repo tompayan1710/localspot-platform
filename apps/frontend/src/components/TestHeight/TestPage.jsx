473a5bc5120b2f2a60e66740a79cb08450de4e83
Tom <tompayan1710@gmail.com>
2025-07-20 13:17:53 +0200

    testing: FalseNavbar

--- apps/frontend/src/components/TestHeight/TestPage.jsx
+++ apps/frontend/src/components/TestHeight/TestPage.jsx
@@ -3,6 +3,9 @@ import "./TestHeight.css"
 export default function TestPage() {
     return (
         <div className={`TestPage`}>
+            <div className="FalseNavBar">
+                <p className="t2">Viarte</p>
+            </div>
             <p className="t32">Ma page de scroll</p>
             {
                 Array.from({length: 10}).map(() => {
--- apps/frontend/src/components/TestHeight/TestPageNav.jsx
+++ apps/frontend/src/components/TestHeight/TestPageNav.jsx
@@ -3,6 +3,9 @@ import "./TestHeight.css"
 export default function TestPageNav() {
     return (
         <div className={`TestPage`}>
+            <div className="FalseNavBar">
+                <p className="t2">Viarte</p>
+            </div>
             <p className="t32">Ma page de scroll avec nav</p>
             {
                 Array.from({length: 10}).map(() => {
